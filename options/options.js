60ffc9415124f82565fd60d132b1db983c249ff1
Crowdbotics <team@crowdbotics.com>
2023-10-03 16:29:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "city-national-bank--43733",
+  url: "https://city-national-bank--43733.botics.co",
+  api: "https://city-national-bank--43733.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
